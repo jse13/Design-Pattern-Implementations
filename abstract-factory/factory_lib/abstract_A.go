68c9373e684cfb450d9bf180d9aab96d853cf023
Julian Engel <julian.engel@navy.mil>
2019-06-21 12:36:32 -0700

    unify abstr product src files

--- abstract-factory/factory_lib/abstract_A.go
+++
@@ -1,6 +0,0 @@
-package factory_lib
-
-// Interface for A products; each family should have it's own concrete implementation of this
-type abstract_A interface {
-	Print()
-}
--- abstract-factory/factory_lib/abstract_products.go
+++ abstract-factory/factory_lib/abstract_products.go
@@ -1,5 +1,10 @@
 package factory_lib
 
+// Interface for A products; each family should have it's own concrete implementation of this
+type abstract_A interface {
+	Print()
+}
+
 // Interface for B products; each family should have it's own concrete implementation of this
 type abstract_B interface {
 	Scale() int
