1e9c4d013533737f78481ebc3c9b86898b7e1526
Julian Engel <julian.engel@navy.mil>
2019-06-21 12:44:45 -0700

    unify family 1 product src files

--- abstract-factory/factory_lib/family_1_products.go
+++ abstract-factory/factory_lib/family_1_products.go
@@ -9,3 +9,12 @@ type A1 struct {
 func ( a1 A1 ) Print() {
 	fmt.Println( "A1" )
 }
+
+// Concrete implementation of the B product that's part of family 1
+type B1 struct {
+}
+
+func ( b1 B1 ) Scale() int {
+	fmt.Println( "B1" )
+	return 2
+}
